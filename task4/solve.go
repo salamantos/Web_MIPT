045107bf5c5dadbf58ce55f05670304489b3ebc3
salamantos <m.b.volkov@yandex.ru>
2017-11-28 23:25:08 +0300

    Task4 completed

--- task4/solve.go
+++ task4/solve.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
-func main() {
-	fmt.Println(fmt.Println(`Hello World`))
+func RemoveEven(array []int) (result []int) {
+	for _, elem := range array {
+		if elem%2 == 0 {
+			result = append(result, elem)
+		}
+	}
+	return
 }
+
+func PowerGenerator(n int) (func() int) {
+	var previousRes = 1
+	return func() (res int) {
+		res = previousRes * n
+		previousRes = res
+		return
+	}
+}
+
+func DifferentWordsCount(str string) (res int) {
+	var word = ""
+	set := make(map[string]bool)
+	res = 0
+
+	for _, symb := range str + " " {
+		if unicode.IsLetter(symb) {
+			word += string(unicode.ToLower(symb))
+		} else if word != "" {
+			if !set[word] {
+				res += 1
+			}
+			set[word] = true
+			word = ""
+		}
+	}
+	return
+}
+
+//func main() {
+//	//input := []int{0, 3, 2, 5}
+//	//result := RemoveEven(input)
+//	//fmt.Println(input, result) // Должно напечататься [3 5]
+//
+//	//gen := PowerGenerator(2)
+//	//fmt.Println(gen()) // Должно напечатать 2
+//	//fmt.Println(gen()) // Должно напечатать 4
+//	//fmt.Println(gen()) // Должно напечатать 8
+//
+//	//fmt.Println(DifferentWordsCount("Hello, world!HELLO  wOrlD...12")) // Должно напечатать 2
+//}
